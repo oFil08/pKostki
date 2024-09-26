a12c9b14597e5de9740818f6373c24f78dcde985
Filakiewicz.jr <oliwierfilak08@gmail.com>
2024-09-26 20:10:25 +0200

    Add files via upload

--- kostki.py
+++ kostki.py
@@ -1,18 +1,33 @@
 import random
-import string
+import re
 
-def arrSum(arr):
-    w = 0
-    for e in arr:
-        w += e
-    return str(w)
+def roll_dice(roll_expression):
+    dice_parts = re.split(r'\s*[\+]\s*', roll_expression)
+    results = []
+    total_sum = 0
+    
+    for part in dice_parts:
+        if "d" in part:
+            dice = part.split("d")
+            if int(dice[0]) > 0:
+                dice_rolls = [random.randint(1, int(dice[1])) for _ in range(int(dice[0]))]
+                results.append("(" + " + ".join(map(str, dice_rolls)) + ")")
+                total_sum += sum(dice_rolls)
+            elif int(dice[0]) < 0:
+                dice_rolls = [random.randint(1, int(dice[1])) for _ in range(abs(int(dice[0])))]
+                results.append("- (" + " + ".join(map(str, dice_rolls)) + ")")
+                total_sum -= sum(dice_rolls)
+        else:
+            results.append(part)
+            total_sum += int(part);
+    return results, total_sum
 
-polecenie = input()
-iloscKostek = int(polecenie[polecenie.index("d")-1])
-rodzajKostek = int(polecenie[polecenie.index("d")+1])
-kostki = []
+while True:
+    expression = input("Dice to roll: \n")
 
-for i in range(iloscKostek):
-    kostki.append(random.randint(1 , rodzajKostek))
+    if expression.lower() == 'exit':
+        break
 
-print(*kostki, sep = ' + ' + ' = '+arrSum(kostki))
+    results, total_sum = roll_dice(expression)
+
+    print(" + ".join(results).replace("+ - ","- "), '=', total_sum)
